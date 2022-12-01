079e43476f2f4cc48d5bc005e186bd5e9ccd1ac3
Christopher <chris.hem10@gmail.com>
2022-12-01 12:36:50 -0600

    Time: 151 ms (34.30%), Space: 13.5 MB (98.71%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -6,15 +6,15 @@ class Solution(object):
         """
         
         # One liner Solution #
-        # return nums+nums
+        return nums+nums
         
         
         # For Loop Solution #
-        result = []
-        n = len(nums)
+#         result = []
+#         n = len(nums)
         
-        for i in range(2 * n):
-            result.append(nums[i%n])
-        return result
+#         for i in range(2 * n):
+#             result.append(nums[i%n])
+#         return result
     
         
