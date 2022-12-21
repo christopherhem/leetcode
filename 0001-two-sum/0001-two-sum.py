5c512db622735e5ffd8950253edf0188af60e050
Christopher <chris.hem10@gmail.com>
2022-12-20 23:45:17 -0600

    Time: 144 ms (45.22%), Space: 15.2 MB (25.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i, value in enumerate(nums):
-            remaining = target - nums[i]  
-            if remaining in d:
-                return[d[remaining], i]
-            d[value]= i
-        
+            diff = target - value 
+            if diff in d:
+                return [d[diff], i]
+            d[value] = i
+            
+            
